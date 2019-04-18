783f958f236cb39f8454bf3f7d2ec3d22da7c410
YuiseiMaruyama <yuisei.rock@gmail.com>
2019-04-18 13:24:24 +0900

    add new file

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
 'use strict'
 
+// 0,1,2のランダム整数値を生成
+// Math.floor(Math.random(()*3))
+
+// 0 ... nのランダム整数値を生成
+// Math.floor(Math.random() * (n+1))
+
+// min ... maxのランダム整数値を生成
+// min + Math.floor(Math.random() * (max + 1 - min))
+
 {
     const btn = document.getElementById('btn');
 
