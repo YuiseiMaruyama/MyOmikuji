17c8917d6fa94be21b5c75ccd33979709be46a6a
YuiseiMaruyama <yuisei.rock@gmail.com>
2019-04-18 13:38:42 +0900

    add new file*

--- js/main.js
+++ js/main.js
@@ -13,24 +13,17 @@
     const btn = document.getElementById('btn');
 
     btn.addEventListener('click', ()=>{
-        const results = ['大吉','中吉','凶','末吉'];
-        const n = Math.floor(Math.random() * results.length);
-        btn.textContent = results[n];
-        // btn.textContent = n;
-        // switch(n){
-        //     case 0:
-        //     btn.textContent = '大吉';
-        //     break;
-
-        //     case 1:
-        //     btn.textContent = '中吉';
-        //     break;
-
-        //     case 2:
-        //     btn.textContent = '凶';
-        //     break;
-        // }
-        // btn.classList.toggle('pressed');
+        // const results = ['大吉','中吉','凶','末吉'];
+        // const n = Math.floor(Math.random() * results.length);
+        // btn.textContent = results[n];
+       const n = Math.random();
+       if(n < 0.05){
+        btn.textContent = '大吉'; //5%
+       }else if(n < 0.2){
+        btn.textContent = '中吉'; //15%
+       }else{
+        btn.textContent = '凶'; //80%
+       }
     });
     btn.addEventListener('mousedown', ()=>{
         btn.classList.add('pressed');
